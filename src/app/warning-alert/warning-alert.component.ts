6f05a84ce6dc48a35622af334d5d98ba2618596d
Shambhavi <shambhavi.rymec@gmail.com>
2023-01-04 16:00:25 +0530

    angular first assignment

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styles: [
+    `
+      h3 {
+        color: red;
+      }
+    `,
+  ],
+})
+export class WarningAlertComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
