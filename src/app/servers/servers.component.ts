31caabac390a50c2b3598cc66caa4d090fdafd8d
Shambhavi <shambhavi.rymec@gmail.com>
2023-01-04 17:41:09 +0530

    event & two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server!';
+  serverName = 'Testing purpose';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server created';
+  }
+
+  onUpdate(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
