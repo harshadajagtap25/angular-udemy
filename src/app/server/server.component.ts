a1c1f500c91318971ebd3c96f9ba19ce08c545d9
Shambhavi <shambhavi.rymec@gmail.com>
2023-01-05 12:09:12 +0530

    ng for

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,12 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
   serverId = 10;
-  serverStatus = 'offline';
+  serverStatus = Math.random() > 0.5 ? 'offline' : 'online';
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'offline' ? 'red' : 'green';
   }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverName = 'Testing purpose';
   servername2 = '';
   serverCreated2 = false;
+  serverArray = ['test1', 'test2'];
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
+    this.serverArray.push(this.serverName)
     this.serverCreationStatus = 'Server created! Name is ' + this.serverName;
   }
 
